607f20d8a324c34ba024fdce379772e33a19de1d
davidmontgomery <davidmontgomeryt@gmail.com>
2015-03-13 05:30:09 +0800

    f

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "pypy"
+maintainer       "Rtb"
+maintainer_email "nobdy"
+license          "Apache 2.0"
+description      "Installs logio"
+version          "1.0.0"
+%w{ubuntu}.each do |os|
+  supports os
+end
