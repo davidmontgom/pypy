3e2b0c28bfc7c022632806b3979105200efcd79d
davidmontgomery <davidmontgomeryt@gmail.com>
2015-03-13 05:29:27 +0800

    f

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,2 +1,21 @@
-
-  
+version = '2.5.0'
+bash "install_pypy" do
+  cwd "#{Chef::Config[:file_cache_path]}"
+  code <<-EOH
+    wget https://bitbucket.org/pypy/pypy/downloads/pypy-#{version}-linux64.tar.bz2
+    tar jxf pypy-#{version}-linux64.tar.bz2
+    mv pypy-#{version}-linux64 /opt/
+    
+    
+    cd /opt/pypy-#{version}-linux64/site-packages/
+    wget http://nightly.ziade.org/distribute_setup.py
+    /opt/pypy-#{version}-linux64/bin/pypy distribute_setup.py
+    
+    cd /opt/pypy-#{version}-linux64/bin
+    curl -O https://bootstrap.pypa.io/get-pip.py
+    /opt/pypy-#{version}-linux64/bin/pypy get-pip.py
+    rm get-pip.py
+    #ln -s /opt/#{version}/bin/pypy /usr/bin/pypy
+  EOH
+  not_if {File.exists?("#{Chef::Config[:file_cache_path]}/pypy-#{version}-linux64.tar.bz2")}
+end
